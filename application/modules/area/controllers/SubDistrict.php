6cccb0f9506a023d468fc976c9eb4e70f5c744ce
Matius Apry Prastowo <mr.apry.prastowo@gmail.com>
2018-08-19 15:38:40 +0700

    initial new area and fixing all bug mirror 1

--- application/modules/area/controllers/SubDistrict.php
+++ application/modules/area/controllers/SubDistrict.php
@@ -1,48 +1,45 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Villages extends MX_Controller 
+class SubDistrict extends MX_Controller
 {
 
     public function __construct()
     {
-        $this->load->model("VillagesModel", "villages");
+        $this->load->model("SubDistrictModel", "SubDistrict");
     }
 
-    public function getVillages()
+    public function getSubDistrict()
     {
         isAjax();
         $getRequest = $this->input->get();
-        $p = $this->villages->get($getRequest);
+        $p = $this->SubDistrict->get($getRequest);
         $p->draw = (int)$getRequest['draw'];
         $p->recordsTotal = $p->data->totalElements;
         $p->recordsFiltered = $p->data->totalElements;
         echo json_encode($p);
     }
 
-    public function saveVillages()
+    public function saveSubDistrict()
     {
         isAjax();
         $data = $this->input->post('data');
-        $p = $this->villages->post($data);
+        $p = $this->SubDistrict->post($data);
         echo json_encode($p);
     }
 
-    public function editVillages()
+    public function editSubDistrict()
     {
         isAjax();
         $data= $this->input->post('data');
-        $p = $this->villages->put($data);
+        $p = $this->SubDistrict->put($data);
         echo json_encode($p);
     }
 
-    public function deleteVillages()
+    public function deleteSubDistrict()
     {
         isAjax();
         $data = $this->input->post('data');
-        $p = $this->villages->delete($data);
+        $p = $this->SubDistrict->delete($data);
         echo json_encode($p);
     }
 }
-
-/* End of file Villages.php */
